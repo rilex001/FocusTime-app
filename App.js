1dc6f716c78e7ce8bc977ddd1b5ec73535a6916a
rile001 <risticza@gmail.com>
2021-08-04 15:21:31 +0200

    Countdown

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Focus } from './src/features/focus/Focus'
+import { colors } from './src/utils/colors';
+import { Timer } from './src/features/timer/Timer';
+import { spacing } from './src/utils/sizes';
+
+
+export default function App() {
+  const [focusSubject, setFocusSubject] = useState("gardening")
+
+  return (
+    <View style={styles.container}>
+      { focusSubject ? (
+        <Timer 
+          focusSubject={focusSubject} 
+          onTimerEnd={() => {
+          setFocusSubject(null)
+        }} />
+      )  : (
+        <Focus addSubject={setFocusSubject} />
+      ) }
+      <Text>{focusSubject}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
+    backgroundColor: colors.darkBlue
+  },
+});
